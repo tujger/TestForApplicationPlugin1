d41a2baa976783e96905ecf21d33edba97cf91b9
eduardm <eduardm@pantheon-inc.com>
2017-08-16 17:54:10 -0400

    Initial commit

---
+++ app/src/main/java/com/edeqa/testforapplicationplugin1/PluginService.java
@@ -0,0 +1,12 @@
+package com.edeqa.testforapplicationplugin1;
+
+/**
+ * Created 8/16/2017.
+ */
+
+public class PluginService extends com.edeqa.waytous.PluginService {
+    static final String LOG_TAG = "ResPluginService1";
+
+}
+
+
