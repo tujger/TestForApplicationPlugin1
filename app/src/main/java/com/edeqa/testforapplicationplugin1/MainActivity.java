23fd176f2a567fa64d1e604085ff9013de41d2b5
eduardm <eduardm@pantheon-inc.com>
2017-08-17 17:07:03 -0400

    Interactions

--- app/src/main/java/com/edeqa/testforapplicationplugin1/MainActivity.java
+++ app/src/main/java/com/edeqa/testforapplicationplugin1/MainActivity.java
@@ -1,7 +1,13 @@
 package com.edeqa.testforapplicationplugin1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.edeqa.waytous.PluginService.ACTION_POST_EVENT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bt_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = ((EditText)findViewById(R.id.et_text)).getText().toString();
+                ((TextView)findViewById(R.id.tv_text)).setText(text);
+
+                Intent intent = new Intent(ACTION_POST_EVENT);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.putExtra("eventName", "change_number");
+                intent.putExtra("eventObject", text);
+                sendBroadcast(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/edeqa/testforapplicationplugin1/PluginService.java
+++ app/src/main/java/com/edeqa/testforapplicationplugin1/PluginService.java
@@ -1,11 +1,48 @@
 package com.edeqa.testforapplicationplugin1;
 
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * Created 8/16/2017.
  */
 
 public class PluginService extends com.edeqa.waytous.PluginService {
     static final String LOG_TAG = "ResPluginService1";
+
+//    @Override
+//    public String getType() {
+//        return getClass().getSimpleName();
+//    }
+
+    @Override
+    public boolean onEvent(String s, Object o) {
+        switch(s) {
+            case "test_event":
+                Log.w(LOG_TAG, "TEST_EVENT:" + o);
+                break;
+            default:
+                Log.w(LOG_TAG, s + ":" + o);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public int getViewResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void viewClicked(int resId) {
+        System.out.println("PLUGIN1:"+resId);
+
+        Intent intent = new Intent(com.edeqa.waytous.PluginService.ACTION_POST_EVENT);
+        intent.putExtra(ACTION_POST_EVENT_NAME, "event_name_from_plugin_one");
+        intent.putExtra(ACTION_POST_EVENT_OBJECT, "event_object_from_plugin_one");
+        getApplicationContext().sendBroadcast(intent);
+
+    }
 
 }
 
